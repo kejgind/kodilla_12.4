0c0e567c1e4b0b4ebd25c6e16f95998dd33a6746
Krzysiek <kejgi@op.pl>
2017-08-25 16:49:19 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+// scripts.js
+
+"use strict";
+
+var url = 'http://api.icndb.com/jokes/random';
+
+getJoke();
+
+var button = document.getElementById('get-joke');
+button.addEventListener('click', function(){
+  getJoke();
+});
+
+var paragraph = document.getElementById('joke');
+
+function getJoke() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.addEventListener('load', function(){
+    var response = JSON.parse(xhr.response);
+    paragraph.innerHTML = response.value.joke;
+  });
+  xhr.send();
+}
